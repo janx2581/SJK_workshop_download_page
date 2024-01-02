bd1de05827fd49b9d755a26afb342a5d26f28829
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-01-03 00:09:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import os
+
+
+def main():
+    st.title("AI workshop SJK")
+
+    # Display the image
+    image_path = "/Users/janhoegh/Desktop/streamlit_download_site/SJK_logo.png"
+    if os.path.exists(image_path):
+        st.image(image_path, width=250)
+    else:
+        st.error("Image not found")
+
+    # Paths to the specific files
+    files = [
+        "/Users/janhoegh/Desktop/streamlit_download_site/Download_files/Ford_The_Changing_Cleavage_Politics_of_Western_Europe.pdf",
+        "/Users/janhoegh/Desktop/streamlit_download_site/Download_files/Ryan_and_Ehlinger_Issue_Publics.pdf"
+    ]
+
+    st.write(
+        'Download disse to filer og find ud af hvordan Fords fund af nye politiske skillelinjer kan bruges til at forstå Ryan og Ehlingers koncept "Issue Publics"')
+
+    # Display download buttons for each file
+    for file_path in files:
+        if os.path.exists(file_path):
+            file_name = os.path.basename(file_path)
+            button_label = f"Klik her for at downloade \n{file_name}"
+            with open(file_path, "rb") as file:
+                st.download_button(
+                    label=button_label,
+                    data=file,
+                    file_name=file_name,
+                    mime="application/pdf"
+                )
+
+    # Add a small text at the bottom of the page
+    st.markdown("_______________________")
+
+    st.write(
+        'Download disse mødenotater for at prøve din GPT')
+
+    # Additional file for download
+    additional_file_path = "/Users/janhoegh/Desktop/streamlit_download_site/Download_files/ClosedAI_mødenotater_mock_up.docx"
+    if os.path.exists(additional_file_path):
+        additional_file_name = os.path.basename(additional_file_path)
+        additional_button_label = f"Klik her for at downloade \n{additional_file_name}"
+        with open(additional_file_path, "rb") as additional_file:
+            st.download_button(
+                label=additional_button_label,
+                data=additional_file,
+                file_name=additional_file_name,
+                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            )
+    else:
+        st.error("Additional file not found!")
+
+    st.markdown("_______________________")
+    st.markdown("_Jan Høegh Consulting_")
+
+
+if __name__ == "__main__":
+    main()
