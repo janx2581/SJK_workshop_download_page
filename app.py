6136afc5a30154e4fdb66735f2e688ab139e9bf4
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-12-02 21:25:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def jan_drafting_prompt():
         'Combine the Goals and solution to a single slide and write out the slides for me as well as a description of visual elements \n')
 
     st.header(
-        'Iteration 2'
+        'Iteration 2')
         
     st.markdown(
         'I am a great presenter, and I can capture a rooms attention. Make it less wordy\n')
